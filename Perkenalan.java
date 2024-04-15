f43203498936b3e071e682009532e9722d1dd956
rizkimaulna <167125649+rizkimaulna@users.noreply.github.com>
2024-04-16 01:45:10 +0700

    Delete Perkenalan.java

--- Perkenalan.java
+++
@@ -1,5 +0,0 @@
-public class Perkenalan {
-        public static void main(String[] args) {
-            System.out.println("Nama\t: Rizki Maulana\nNIM\t: 1237050088\nKelas\t: IF 2-D");
-        }
-}
